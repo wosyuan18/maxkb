4646be104448f3cc766d3fa1891c561bfd3d9442
wosyuan18 <wosyuan18@qq.com>
2025-06-01 17:41:55 +0800

    Update dataset_serializers.py

--- apps/dataset/serializers/dataset_serializers.py
+++ apps/dataset/serializers/dataset_serializers.py
@@ -411,9 +411,9 @@ class DataSetSerializers(serializers.ModelSerializer):
                                 'embedding_mode_id': instance.get('embedding_mode_id')}
             return self.save(dataset_instance, with_valid=True)
 
-        @valid_license(model=DataSet, count=-1,
+        @valid_license(model=DataSet, count=9999,
                        message=_(
-                           'The community version supports up to 50 knowledge bases. If you need more knowledge bases, please contact us (https://fit2cloud.com/).'))
+                           'The community version supports up to 9999 knowledge bases. If you need more knowledge bases, please contact us (https://fit2cloud.com/).'))
         @post(post_function=post_embedding_dataset)
         @transaction.atomic
         def save(self, instance: Dict, with_valid=True):
